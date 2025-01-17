b1f27c9347070bc71e5d56974104b62a2279015f
Yassine Amri <yam@exerp.com>
2025-01-17 11:15:45 +0100

    Minimal test

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,11 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  fixturesFolder: false,
+
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
-}
+});
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,17 @@
-/// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  it('it works', () => {
+    cy.visit('index.html')
+    cy.viewport(600, 400)
+    const viewportWidth = Cypress.config('viewportWidth')
+    // cy.log(`viewportWidth: ${viewportWidth}`) // viewportWidth: 1000
+    expect(viewportWidth).to.equal(1000)
+  })
+
+  it('it does not work', () => {
+    cy.visit('index.html')
+    cy.viewport(600, 400)
+    const viewportWidth = Cypress.config('viewportWidth')
+    // cy.log(`viewportWidth: ${viewportWidth}`) // viewportWidth: 1000
+    expect(viewportWidth).to.equal(600)
   })
 })
